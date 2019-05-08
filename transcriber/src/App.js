25ce969e2cb031fe5a48b93db67f9dd3db51567a
David Kamholz <lautgesetz@gmail.com>
2019-05-07 18:09:20 -0700

    add getRegionUrl

--- transcriber/src/App.js
+++ transcriber/src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 
 import Keyboard from './Keyboard';
 
-let entryImageUrl = window.entryImageUrl || "https://archive.org/download/tutur-smara-bhuwana/page/leaf1.jpg";
+let entryImageUrl = window.entryImageUrl;
+
+let iiifBaseUrl = 'https://iiif.archivelab.org/iiif/';
 
 export default class App extends Component {
   constructor(props) {
@@ -52,15 +54,46 @@ export default class App extends Component {
     }, this.scrollTextDown);
   }
 
+  getRegionUrl = () => {
+    let url = iiifBaseUrl;
+    let textbox = document.getElementById("wpTextbox1");
+
+    let matches = textbox.value.match(/EntryID=(.+)/);
+    if (!matches) return null;
+    url += matches[1];
+
+    matches = textbox.value.match(/Title=(.+)/);
+    if (!matches) return null;
+    url += `%24${matches[1]}/`;
+
+    // TODO: do this properly through React
+    matches = img.getAttribute('style').match(/translate3d\(-([0-9.]+)px, -([0-9.]+)px, 0px\) scale\(([0-9.]+)\)/);
+    if (!matches) return null;
+    let xOffset = matches[1];
+    let yOffset = matches[2];
+    let scale = matches[3];
+
+    let containerRect = document.getElementById("image-container").getBoundingClientRect();
+    let img = document.getElementById("lontar");
+
+    let xPct = (100 * xOffset / (img.width * scale)).toFixed(2);
+    let yPct = (100 * yOffset / (img.height * scale)).toFixed(2);
+    let widthPct = (100 * containerRect.width / (img.width * scale)).toFixed(2);
+    let heightPct = (100 * containerRect.height / (img.height * scale)).toFixed(2);
+
+    url += `pct:${xPct},${yPct},${widthPct},${heightPct}/full/0/default.jpg`;
+    return url;
+  }
+
   render() {
     return (
       <div className="App">
         {this.state.open &&
           <div className="transcriber">
             <meta name="viewport" content="width=device-width, user-scalable=no" />
-            <div className="image-container">
+            <div id="image-container" className="image-container">
               <PinchZoomPan maxScale={5} doubleTapBehavior="zoom">
-                <img alt="lontar" src={entryImageUrl} />
+                <img id="lontar" alt="lontar" src={entryImageUrl} />
               </PinchZoomPan>
             </div>
             <div className="text" onClick={this.handleCaretMove} ref={this.textRef}>
