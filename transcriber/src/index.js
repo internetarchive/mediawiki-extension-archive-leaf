d3fd882319de62c334ac8df1ef28a62052a3a99e
David Kamholz <lautgesetz@gmail.com>
2019-05-12 22:33:14 -0700

    inject transcriber at top of body

--- transcriber/src/index.js
+++ transcriber/src/index.js
@@ -5,7 +5,7 @@ import * as serviceWorker from './serviceWorker';
 
 let transcriber = document.createElement("div");
 transcriber.id = "transcriber";
-document.body.appendChild(transcriber);
+document.body.insertBefore(transcriber, document.body.firstChild);
 ReactDOM.render(<App />, transcriber);
 
 // If you want your app to work offline and load faster, you can change
