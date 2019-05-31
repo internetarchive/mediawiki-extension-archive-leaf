f83c31721b3790932be72a144c63b435b0cea3e5
David Kamholz <lautgesetz@gmail.com>
2019-05-31 14:28:25 -0700

    additional cleanup/refactoring

--- transcriber/src/App.js
+++ transcriber/src/App.js
@@ -8,43 +8,72 @@ import { faTimes, faKeyboard, faFont } from '@fortawesome/free-solid-svg-icons';
 import styles from './App.module.scss';
 import Keyboard from './Keyboard';
 
-let entryImageUrl = window.entryImageUrl;
-let iiifBaseUrl = "https://iiif.archivelab.org/iiif";
+const iiifBaseUrl = "https://iiif.archivelab.org/iiif";
+const transliteratorUrl = "https://bali.panlex.org/transliterate";
+const platform = detectPlatform();
+const getSelection = detectGetSelection();
 
-const blockPinchZoom = e => {
+viewportFix();
+
+function detectPlatform() {
+  let ua = window.navigator.userAgent;
+  let platform = {};
+  platform.iOS = ua.match(/iPhone|iPod|iPad/);
+  platform.iOSSafari = platform.iOS && ua.match(/WebKit/) && !ua.match(/CriOS/);
+  platform.mobile = platform.iOS || ua.match(/Android/);
+  return platform;
+}
+
+function detectGetSelection() {
+  if (platform.iOS) {
+    if (document.caretRangeFromPoint) {
+      return e => {
+        let range = document.caretRangeFromPoint(e.clientX, e.clientY);
+        return range && range.collapsed
+          && { node: range.commonAncestorContainer, caretPos: range.startOffset };
+      };
+    }
+  } else {
+    if (window.getSelection) {
+      return () => {
+        let sel = window.getSelection();
+        return sel.anchorNode && sel.isCollapsed
+          && { node: sel.anchorNode, caretPos: sel.anchorOffset };
+      };
+    }
+  }
+}
+
+function viewportFix() {
+  if (platform.mobile) {
+    document.documentElement.style.setProperty("--vh", getVhPx());
+    window.addEventListener("resize", () => {
+      document.documentElement.style.setProperty("--vh", getVhPx());
+    });
+  }
+}
+
+function getVhPx() {
+  let height = document.documentElement.clientHeight;
+  return (height * 0.01) + "px";
+}
+
+function blockPinchZoom(e) {
   if (e.touches.length > 1) {
     e.preventDefault();
   }
 }
 
-// const blockTapZoom = e => {
+// function blockTapZoom(e) {
 //   e.preventDefault();
 //   e.target.click();
 // }
-
-const getSelection = () => {
-  let sel = window.getSelection();
-  return sel.anchorNode && sel.isCollapsed
-    ? { node: sel.anchorNode, caretPos: sel.anchorOffset }
-    : null;
-}
-
-const getCaretRangeFromPoint = e => {
-  let range = document.caretRangeFromPoint(e.clientX, e.clientY);
-  return range && range.collapsed
-    ? { node: range.commonAncestorContainer, caretPos: range.startOffset }
-    : null;
-}
 
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.caretRef = React.createRef();
     this.textbox = document.getElementById("wpTextbox1");
-
-    this.detectPlatform();
-    this.detectGetSelection();
-    this.viewportFix();
 
     this.state = {
       text: "",
@@ -54,38 +83,8 @@ export default class App extends Component {
       transliteration: "",
       transliterationOpen: false,
       keyboardOpen: !(window.localStorage.getItem("keyboardOpen") === "false"),
+      imageUrl: window.entryImageUrl,
     };
-  }
-
-  detectPlatform = () => {
-    let ua = window.navigator.userAgent;
-    this.isIOS = ua.match(/iPhone|iPod|iPad/);
-    this.isIOSSafari = this.isIOS && ua.match(/WebKit/) && !ua.match(/CriOS/);
-    this.isMobile = this.isIOS || ua.match(/Android/);
-  }
-
-  detectGetSelection = () => {
-    let [ supported, func ] = this.isIOS
-      ? [ document.caretRangeFromPoint, getCaretRangeFromPoint ]
-      : [ window.getSelection, getSelection ];
-
-    this.getSelection = supported
-      ? func
-      : () => null;
-  }
-
-  viewportFix = () => {
-    if (this.isMobile) {
-      document.documentElement.style.setProperty("--vh", this.getVhPx());
-      window.addEventListener("resize", () => {
-        document.documentElement.style.setProperty("--vh", this.getVhPx());
-      });
-    }
-  }
-
-  getVhPx = () => {
-    let height = document.documentElement.clientHeight;
-    return (height * 0.01) + "px";
   }
 
   componentDidMount = () => {
@@ -118,7 +117,7 @@ export default class App extends Component {
 
   afterOpen = () => {
     if (!this.error) {
-      if (this.isIOSSafari) {
+      if (platform.iOSSafari) {
         document.addEventListener("touchmove", blockPinchZoom, { passive: false });
         //document.addEventListener("touchend", blockTapZoom, { passive: false });
       }
@@ -131,7 +130,7 @@ export default class App extends Component {
   }
 
   handleClose = () => {
-    if (this.isIOSSafari) {
+    if (platform.iOSSafari) {
       document.removeEventListener("touchmove", blockPinchZoom);
       //document.removeEventListener("touchend", blockTapZoom);
     }
@@ -214,7 +213,7 @@ export default class App extends Component {
   }
 
   handleCaretMove = e => {
-    let sel = this.getSelection(e);
+    let sel = getSelection && getSelection(e);
     if (sel) {
       let { node, caretPos } = sel;
 
@@ -261,17 +260,13 @@ export default class App extends Component {
 
   getTransliteration = () => {
     return new Promise((resolve, reject) => {
-      window.fetch("https://bali.panlex.org/transliterate", {
+      window.fetch(transliteratorUrl, {
         method: "POST",
         body: this.state.text
       }).then(res => {
         res.text().then(resolve, reject);
       }, reject);
     });
-  }
-
-  imageChange = state => {
-    this.imageState = state;
   }
 
   getImageRegionUrl = () => {
@@ -293,8 +288,13 @@ export default class App extends Component {
       <div className={styles.App}>
         <div className={cx(styles.transcriber, (!this.state.open || this.state.error) && styles.closed)}>
           <div className={cx(styles.image, !this.state.keyboardOpen && styles.expanded)}>
-            <PinchZoomPan maxScale={5} doubleTapBehavior="zoom" zoomButtons={!this.isMobile} onChange={this.imageChange}>
-              <img id="lontar" alt="lontar" src={entryImageUrl} />
+            <PinchZoomPan
+              maxScale={5}
+              doubleTapBehavior="zoom"
+              zoomButtons={!platform.mobile}
+              onChange={state => this.imageState = state}
+            >
+              <img id="lontar" alt="lontar" src={this.state.imageUrl} />
             </PinchZoomPan>
           </div>
           {this.state.keyboardOpen ?
@@ -317,7 +317,7 @@ export default class App extends Component {
           }
           <div
             className={cx(styles.transliteration, this.state.transliterationOpen && styles.visible, !this.state.keyboardOpen && styles.expanded)}
-            onClick={this.isMobile && this.hideTransliteration}
+            onClick={platform.mobile && this.hideTransliteration}
           >
             <div className={styles.transliterationText}>
               {this.state.transliteration}
@@ -334,7 +334,7 @@ export default class App extends Component {
         </div>
         {(this.state.open && !this.state.error) ?
           <div className={styles.buttons}>
-            {!this.isMobile &&
+            {!platform.mobile &&
               <>
                 <button
                   className={styles.button}
